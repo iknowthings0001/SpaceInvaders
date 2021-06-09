46e9cc434258781ada2019a77fe049aaeab33718
Iknowthings <Iknowthings0001@gmail.com>
2021-06-09 06:58:38 -0500

    Adding update

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import pygame as pg
 
 
-pg.init()
 WIDTH=800
 HEIGHT=800
 color_screen = (127,127,127)
-screen = pg.display.set_mode([WIDTH, HEIGHT])
 cradius=75 
 FPS=60
+
+
+pg.init()
+screen = pg.display.set_mode([WIDTH, HEIGHT])
+
 
 def draw_screen(circle):
     screen.fill(color_screen)
     pg.draw.circle(screen,(0,0,255),(circle.x,circle.y),cradius)
-    pg.display.flip()
+    pg.display.update()
 
 
 circle=pg.Rect(WIDTH/2, HEIGHT/2,cradius,cradius)
